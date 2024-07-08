bb35616cfef9c77863422ee5c160e7d366acb3e5
dayong <wangdayong228@163.com>
2024-07-08 16:40:43 +0800

    nil

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"log"
 
 	"github.com/conflux-fans/cmctool/internal/configs"
+	"github.com/conflux-fans/cmctool/internal/servicies/volume"
 )
 
 func main() {
 	configs.Init()
 	log.Println("config initialization done")
+	volume.Loop()
+	select {}
 }
